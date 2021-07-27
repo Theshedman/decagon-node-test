a12d947e916f6db16d1c5f973a5ffc6ec6fda661
Shedrack Ajaegbu <shedrack3@gmail.com>
2021-07-27 06:35:07 +0100

    Add a utility for normalizing trips bill

---
+++ src/utils/index.js
@@ -0,0 +1,11 @@
+/**
+ * Analytics utility module
+ * @alias module:utils/analytics
+ */
+module.exports = require('./analytics.js');
+
+/**
+ * NormalizeAmount utility module
+ * @alias module:utils/normalizeAmount
+ */
+module.exports = require('./normalizeAmount.js');
---
+++ src/utils/normalizeAmount.js
@@ -0,0 +1,21 @@
+/**
+ * Utility function for normalizing trips amount
+ * @module utils/normalizeAmount
+ */
+
+/**
+ * Normalize amount
+ * @param { string } amount - trip amount
+ * @return { number }
+ */
+
+function normalizeAmount(amount) {
+  // Handle the edge case when the amount is not of type string
+  if (typeof amount !== 'string') {
+    amount = String(amount);
+  }
+
+  return Number(amount.split(',').join(''));
+}
+
+module.exports = normalizeAmount;
