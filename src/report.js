3b9e3aaabfddfb1d192c6b149358524a3da87554
Shedrack Ajaegbu <shedrack3@gmail.com>
2021-07-28 02:56:38 +0100

    Add functionality for the report module

--- src/report.js
+++ src/report.js
@@ -1,10 +1,97 @@
 const { getTrips, getDriver, getVehicle } = require('api');
+const { Analytics, normalizeAmount } = require('./utils');
 
 /**
  * This function should return the data for drivers in the specified format
  *
  * @returns {any} Driver report data
  */
-async function driverReport() {}
+async function driverReport() {
+  try {
+    const trips = await getTrips();
+    const driverTrips = Analytics.groupTripsByDriverId(trips);
+    const analyzedTripData = [];
+
+    for (let driverId of driverTrips.keys()) {
+      const vehicles = [];
+      const tripAnalysis = {
+        noOfCashTrips: 0,
+        noOfNonCashTrips: 0,
+        totalAmountEarned: 0,
+        totalCashAmount: 0,
+        totalNonCashAmount: 0,
+        trips: [],
+      };
+      const driverData = {};
+
+      const driverTrip = driverTrips.get(driverId);
+      const noOfTrips = driverTrip.length;
+      let driverDetails;
+
+      // Handle invalid id
+      if (driverId !== '7ba0dc7ba0dce0-1de3-4971-82e0-a10acce52dd2') {
+        driverDetails = await getDriver(driverId);
+        driverData.fullName = driverDetails.name;
+        driverData.id = driverId;
+        driverData.phone = driverDetails.phone;
+
+        for (let vehicleId of driverDetails['vehicleID']) {
+          const vehicle = await getVehicle(vehicleId);
+          vehicles.push({
+            plate: vehicle.plate,
+            manufacturer: vehicle.manufacturer,
+          });
+        }
+      } else {
+        driverData.id = driverId;
+      }
+
+      for (let trip of driverTrip) {
+        if (trip['isCash']) {
+          tripAnalysis.noOfCashTrips += 1;
+          tripAnalysis.totalCashAmount += normalizeAmount(trip['billedAmount']);
+
+        } else {
+          tripAnalysis.noOfNonCashTrips += 1;
+          tripAnalysis.totalNonCashAmount += normalizeAmount(trip['billedAmount']);
+        }
+
+        tripAnalysis.trips.push({
+          user: trip.user.name,
+          created: trip.created,
+          pickup: trip.pickup.address,
+          destination: trip.destination.address,
+          billed: normalizeAmount(trip['billedAmount']),
+          isCash: trip.isCash,
+        });
+      }
+
+      const totalEarnedAmount = tripAnalysis.totalNonCashAmount + tripAnalysis.totalCashAmount;
+      tripAnalysis.totalAmountEarned = normalizeAmount(totalEarnedAmount.toFixed(2));
+
+      const totalCashAmount = tripAnalysis.totalCashAmount;
+      const totalNonCashAmount = tripAnalysis.totalNonCashAmount;
+      tripAnalysis.totalCashAmount = normalizeAmount(
+        totalCashAmount.toFixed(2),
+      );
+      tripAnalysis.totalNonCashAmount = normalizeAmount(
+        totalNonCashAmount.toFixed(2),
+      );
+
+      analyzedTripData.push({
+        ...driverData,
+        noOfTrips,
+        vehicles,
+        ...tripAnalysis,
+      });
+    }
+
+    return analyzedTripData;
+  } catch (e) {
+    throw new Error(e.message);
+  }
+}
+
+driverReport().then();
 
 module.exports = driverReport;
